bd75d26fa1ef672f738d2c28f7c9ca04550d4c2c
Petri Asikainen <petri.asikainen@sataedu.fi>
2021-02-19 07:58:42 +0200

    Return parameter values as lowercase

--- SnipeitPS/Private/Get-ParameterValue.ps1
+++ SnipeitPS/Private/Get-ParameterValue.ps1
@@ -1,24 +1,10 @@
 function Get-ParameterValue {
-    #.Synopsis
-    #  Get the actual values of parameters which have manually set (non-null) default values or values passed in the call
-    #.Description
-    #  Unlike $PSBoundParameters, the hashtable returned from Get-ParameterValues includes non-empty default parameter values.
-    #  NOTE: Default values that are the same as the implied values are ignored (e.g.: empty strings, zero numbers, nulls).
-    #.Example
-    #  function Test-Parameters {
-    #      [CmdletBinding()]
-    #      param(
-    #          $Name = $Env:UserName,
-    #          $Age
-    #      )
-    #      $Parameters = . Get-ParameterValues
+    # returns command parameters as hastable, kays in lowercase
+    # This helps keeping function parameters formated as Titlecase
+    # and usen them directly with snipeit api in lowercase
     #
-    #      # This WILL ALWAYS have a value...
-    #      Write-Host $Parameters["Name"]
-    #
-    #      # But this will NOT always have a value...
-    #      Write-Host $PSBoundParameters["Name"]
-    #  }
+    # Get-ParameterValue must be dot-sourced, like this: . Get-ParameterValue
+    # 
     [CmdletBinding()]
     param(
         # Pass $MyInvocation.MyCommand.Parameters to function, powershell 7 seems to only populate variables with dot sourcing
@@ -30,7 +16,7 @@ function Get-ParameterValue {
     )
 
     if ($MyInvocation.Line[($MyInvocation.OffsetInLine - 1)] -ne '.') {
-        throw "Get-ParameterValue must be dot-sourced, like this: . Get-ParameterValues"
+        throw "Get-ParameterValue must be dot-sourced, like this: . Get-ParameterValue"
     }
     
 
@@ -42,7 +28,7 @@ function Get-ParameterValue {
             if ($key -notin $DefaultExcludeParameter) {
                 if ($null -ne ($value = Get-Variable -Name $key -ValueOnly -ErrorAction Ignore )) {
                     if ($value -ne ($null -as $parameter.Value.ParameterType)) {
-                        $ParameterValues[$key] = $value
+                        $ParameterValues[$key.toLower()] = $value
                     }
                 }
 
